3148f6c999323e7155927fab89a4c8df6be23c68
Mr-Salman-Ansari <salman985604@gmail.com>
2026-03-01 02:11:22 +0530

    update

--- src/main/java/com/my/entity/student.java
+++ src/main/java/com/my/entity/student.java
@@ -1,5 +1,6 @@
 package com.my.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,7 +11,9 @@ public class student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
      private int id;
+	
      private String name, gender;
+     @Column(name="stuName")
      private int age;
      
      
