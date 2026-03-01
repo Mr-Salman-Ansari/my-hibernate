8bf2361ddb54f4c49ec655bcbfcf41062c2d73d3
Mr-Salman-Ansari <salman985604@gmail.com>
2026-03-02 04:12:11 +0530

    update

---
+++ src/main/java/com/my/config/stuConfiguration.java
@@ -0,0 +1,33 @@
+package com.my.config;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+public class stuConfiguration {
+	
+	public static SessionFactory getSessionFactory() {
+		
+		Properties properties = new Properties();
+		
+		properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/college");
+		properties.put(Environment.JAKARTA_JDBC_USER, "root");
+		properties.put(Environment.JAKARTA_JDBC_PASSWORD, "7599");
+		properties.put(Environment.HBM2DDL_AUTO, "create");
+		properties.put(Environment.SHOW_SQL, "true");
+		//properties.put(Environment.FORMAT_SQL, "true");
+		
+		 StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings( properties).build();
+		 Metadata metaData = new MetadataSources(ssr).addAnnotatedClass(com.my.entity.student.class).getMetadataBuilder().build();
+		 SessionFactory sessionFcatory = metaData.buildSessionFactory();
+		
+		return sessionFcatory;
+	}
+
+}
--- src/main/java/com/my/main/studentRunner.java
+++ src/main/java/com/my/main/studentRunner.java
@@ -9,11 +9,12 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import com.my.config.stuConfiguration;
 import com.my.entity.student;
 
 public class studentRunner {
    public static void main(String [] args) {
-//	   student st = new student("Annu","Male",22);
+	   student st = new student("Annu","Male",22);
 	   
 	  // Configuration cfg = new Configuration().configure(); // it is work like a midiator 
 	  // SessionFactory sf = cfg.buildSessionFactory();
@@ -23,23 +24,14 @@ public class studentRunner {
 //	   Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
 //	   
 //	   SessionFactory sf = metadata.buildSessionFactory();
-	   
-	  
-	   SessionFactory sf = new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build())
-			   .getMetadataBuilder().build()
-			   .buildSessionFactory();
-	   
-	   
-	   Session ss = sf.openSession();
+	    
+	   Session ss = stuConfiguration.getSessionFactory().openSession();
 	   Transaction tx = ss.beginTransaction();
 	   
-	   student st = ss.find(student.class, 2);
+//	   student st = ss.find(student.class, 2);
 	  // System.out.println(st);
 	   
-	   
-//	   ss.persist(st);
-//	   tx.commit();
-	   
-   }
-	
+	   ss.persist(st);
+	   tx.commit();
+	   }
 }
